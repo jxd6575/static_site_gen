fa59d280366e29532bcaf9a1e604d2a1b46df1de
jxd6575 <jxd6575@g.rit.edu>
2025-04-24 13:31:08 -0400

    N: Sending up to github pages

--- src/extract_title.py
+++ src/extract_title.py
@@ -13,7 +13,7 @@ def extract_title(markdown):
 
     raise Exception("No h1 '#' header")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_file = open(from_path)
     from_file_read = from_file.read()
@@ -29,6 +29,7 @@ def generate_page(from_path, template_path, dest_path):
     from_file_HTML_string = from_file_HTMLNode.to_html()
     title = extract_title(from_file_read)
     replaced_template = template_file_read.replace("{{ Title }}", title).replace("{{ Content }}", from_file_HTML_string)
+    replaced_template = replaced_template.replace('href="/', f'href="{basepath}').replace('src="/',f'src="{basepath}')
     # Create directory structure if it doesn't exist
     dest_dir = os.path.dirname(dest_path)
     if dest_dir:  # Only try to create if there's a directory part
@@ -41,13 +42,13 @@ def generate_page(from_path, template_path, dest_path):
     #print(from_file_read)
     #print(replaced_template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     #print(os.listdir(dir_path_content))
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,26 +1,28 @@
 import os
 import shutil
-
+import sys
 from copystatic import copy_files_recursive
 from extract_title import generate_page, generate_pages_recursive
 
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_docs = "./docs"
 from_path = "./content/index.md"
 template_path = "./template.html"
 to_path = "./public/index.html"
 dir_path_content = "./content"
-
+basepath = '/'
 def main():
-    print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    print(basepath)
+    print("Deleting docs directory...")
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    copy_files_recursive(dir_path_static, dir_path_docs)
     #generate_page(from_path, template_path, to_path)
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
 
 
 if __name__ == "__main__":
