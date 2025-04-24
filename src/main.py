e9b4660a21c834720eea95822805d967cd0dfb16
jxd6575 <jxd6575@g.rit.edu>
2025-04-24 12:59:38 -0400

    M: Added recursive generator

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
-from extract_title import generate_page
+from extract_title import generate_page, generate_pages_recursive
 
 
 dir_path_static = "./static"
@@ -10,6 +10,7 @@ dir_path_public = "./public"
 from_path = "./content/index.md"
 template_path = "./template.html"
 to_path = "./public/index.html"
+dir_path_content = "./content"
 
 def main():
     print("Deleting public directory...")
@@ -18,7 +19,8 @@ def main():
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
-    generate_page(from_path, template_path, to_path)
+    #generate_page(from_path, template_path, to_path)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 
 if __name__ == "__main__":
